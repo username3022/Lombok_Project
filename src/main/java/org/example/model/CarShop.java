19fed125e2f08d2c7f93354ba12fe910eaa74512
Veselin Dimitrov <69313136+godziwaBG@users.noreply.github.com>
2023-02-01 13:02:32 +0200

    Car shop added

--- src/main/java/org/example/model/CarShop.java
+++ src/main/java/org/example/model/CarShop.java
@@ -1,16 +1,13 @@
 package org.example.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class CarShop extends Car{
+public class CarShop {
     private int price;
     private String color;
     private boolean kidsSeat;
